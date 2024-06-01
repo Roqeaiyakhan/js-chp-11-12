e7dc5b8c541e44ff764812d94dfb679bf476d37e
Roqeaiyakhan <roqaiyajaani@gmail.com>
2024-06-01 22:53:53 +0500

    add pic of chp 11 and 12

---
+++ index.js
@@ -0,0 +1,23 @@
+// var userInputcolor = prompt("enter signal color");
+// if(userInputcolor === green){
+//     alert("ap ja sakty kha")
+// }
+
+// else if(userInputcolor === yellow){
+//     alert("get ready")
+// }
+// else{
+//     alert("stop")
+//}
+
+var  shoopingamount = +prompt("enter your shopping amount")
+
+if(shoopingamount <= 4999 ){
+    alert("10% discount")
+}
+else if(shoopingamount <=9999){
+    alert("25% discount")
+}
+else{
+    alert("50% discount")
+}

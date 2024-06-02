c8fc04a8b3799e371e3ffb474a7e456bd64a3ab7
Roqeaiyakhan <roqaiyajaani@gmail.com>
2024-06-02 18:28:12 +0500

    add practice program

--- index.js
+++ index.js
@@ -10,14 +10,122 @@
 //     alert("stop")
 //}
 
-var  shoopingamount = +prompt("enter your shopping amount")
+// var  shoopingamount = +prompt("enter your shopping amount")
 
-if(shoopingamount <= 4999 ){
-    alert("10% discount")
-}
-else if(shoopingamount <=9999){
-    alert("25% discount")
-}
-else{
-    alert("50% discount")
-}
+// if(shoopingamount <= 4999 ){
+//     alert("10% discount")
+// }
+// else if(shoopingamount <=9999){
+//     alert("25% discount")
+// }
+// else{
+//     alert("50% discount")
+// }
+
+
+//DISCOUNT //
+// var noOfItems = +prompt("ap nay kitny chejy khardi?")
+// var discount;
+
+// if(noOfItems >= 10) {
+//     discount = 20
+// }
+// else if (noOfItems >= 5){
+//     discount = 10
+// }
+// else {
+//     discount = 0
+// }
+
+// console.log(`Your discount is: ${discount}%`)
+   
+//........BAADAL........//
+// var baadal = true;
+// if(!baadal){
+//     console.log("rain expected")
+// }
+//  else{
+//     console.log("Rain not expected")
+//  }
+
+//BRT CARD BALANCE//
+// var cardbalance = +prompt("please your balance")
+// if(cardbalance >= 55){
+//     console.log("welcome to Zu peshawar")
+// }
+// else {
+//     console.log(` your balance is ${cardbalance} please reaharge your balance`)
+// }
+
+//BURGER LENA KHA//
+// var burgerprice = +prompt("how much money in your pocket")
+
+// if(burgerprice >= 350){
+//     console.log(`${burgerprice} kaly burger`)
+// }
+// else{
+//     console.log("jb khali kha")
+// }
+
+
+
+    //program for even odd///
+//  var num = +prompt("please enter your number")
+//  if(num % 2 === 0){
+//     console.log(`${num} is even`)
+//  }
+// else{
+//     console.log(`${num} is odd`)
+// }
+
+//ATM CARD//
+// var balance = 3000
+// var  amount = +prompt("kitne pese niklane kha")
+// if(amount >= balance){
+//     console.log("Transaction succeful.")
+// }
+// else{
+//     console.log("insuffient balance")
+// }
+
+//totalbill for stationary//
+// var  pencil = 10;
+// var notebook = 300;
+// var totalbill = pencil + notebook ;
+// console.log({totalbill})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
